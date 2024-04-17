60a595f2be5eb30f97ee36dbe2319fc269069a95
Jovincia <assogbadanielle15@gmail.com>
2024-04-17 23:55:12 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gesture_detector/home_page.dart';
+import 'package:gesture_detector/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,65 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        canvasColor: MainColors.brownCoffeeMilk,
+       // colorScheme: ColorScheme.fromSeed(color: MainColors.brownCoffeeMilk),
+        //useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const MyHomePage(
+        
+        title: 'Ikiiiii >>>>>>>>>>>>'
+        ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MainColors.brownNude,
+        title: Text(widget.title,textAlign: TextAlign.justify,),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: MainColors.brownCoffeeMilk,
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.book),
+      ),
     );
   }
 }
